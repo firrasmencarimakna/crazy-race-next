a8a886a529068406f788b6ff1a49b406205181e9
k1zuko <muhammadhuda537@gmail.com>
2025-11-25 20:14:05 +0700

    header meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,41 @@
 import type { Metadata } from 'next';
+import { headers } from "next/headers";
 import ClientLayout from './ClientLayout';
 import { Press_Start_2P } from 'next/font/google'
 import './globals.css';
 
-export const metadata: Metadata = {
-  title: "Crazy Race",
-  description: "Answer • Race • Win",
-  manifest: "/manifest.json",
-  icons: {
-    icon: "/icons/icon-192x192.png",
-    shortcut: "/favicon.ico",
-    apple: "/icons/icon-512x512.png",
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const host = headers().get("host") || "crazyrace.gameforsmart.com";
+  const protocol = host.includes("localhost") ? "http" : "https";
+  const fullUrl = `${protocol}://${host}`;
+
+  return {
+    title: "Crazy Race",
+    description: "Answer • Race • Win",
+    openGraph: {
+      title: "Crazy Race",
+      description: "Answer • Race • Win",
+      url: fullUrl,
+      siteName: "Crazy Race",
+      images: [
+        {
+          url: "/icons/icon-512x512.png",
+          width: 512,
+          height: 512,
+          alt: "Crazy Race Logo",
+        }
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Crazy Race",
+      description: "Answer • Race • Win",
+      images: ["/icons/icon-512x512.png"],
+    },
+  };
+}
+
 
 const pressStart = Press_Start_2P({
   subsets: ['latin'],
